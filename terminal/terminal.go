105bcce7dae1f8f55e1c8ae7214e8753866fc934
Timur Makarchuk <makarchuk@group-ib.com>
2017-06-26 20:58:54 +0300

    Added simple exec functionality

--- terminal/terminal.go
+++ terminal/terminal.go
@@ -1,20 +1,29 @@
 package terminal
 
 import (
+  "io"
   "os"
+  "fmt"
+  "bufio"
+  "strings"
   "os/user"
+  "os/exec"
   "strconv"
 )
 
 type Terminal struct {
+  Dir string
 }
 
 func InitTerminal() Terminal {
-  return Terminal{}
+  t := Terminal{ "" }
+  t.Dir = t.Pwd()
+  return t
 } 
 
 func (t Terminal) Invitation() string {
-  return t.Username() + "@" + t.Hostname() + ":" + t.Pwd() + "$ "
+  // TODO: Build from format string. Makre it customizable
+  return t.Username() + "@" + t.Hostname() + ":" + t.Dir + "$ "
 }
 
 func (t Terminal) Hostname() string {
@@ -51,6 +60,45 @@ func (t Terminal) Username() string {
   } else {
     return current_user.Username
   }
-
 }
 
+// TODO: Change string to array of strings and call it like a decent person
+func (t Terminal) RunBinary(binary string, args ...string) {
+  cmd := exec.Command(binary, args...)
+  stdout, _ := cmd.StdoutPipe()
+  err := cmd.Start()
+  if err != nil {
+    fmt.Println(err)
+  }
+  output := bufio.NewReader(stdout)
+  for true {
+    chunk, err := output.ReadString('\n')
+    fmt.Print(chunk)
+    if err != nil {
+      break
+    }
+  }
+}
+
+func (t Terminal) HandleInput(inp string) {
+  chunks := strings.Split(inp, " ")
+  command := chunks[0]
+  args := chunks[1:]
+  t.RunBinary(command, args...)
+}
+
+
+func (t Terminal) Run() {
+  reader := bufio.NewReader(os.Stdin)
+  for true {
+    fmt.Print(t.Invitation())
+    input, err := reader.ReadString('\n')
+    if input == "exit\n" || err == io.EOF {
+      break
+    } else {
+      input = strings.Trim(input, "\n ")
+      t.HandleInput(input)
+    }   
+  }
+  fmt.Println("Bye!")
+}
